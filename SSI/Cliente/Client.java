eef76a4f477cd4ebda38e54837cbc293401f7191
Paulo <Paulo@bl16-46-112.dsl.telepac.pt>
2015-02-28 17:44:25 +0000

    Communication encrypted using RC4 The datagrams are sent immediately without buffering to compleat a block

--- SSI/Cliente/Client.java
+++ SSI/Cliente/Client.java
@@ -3,19 +3,15 @@ package Cliente;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.Socket;
-import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.crypto.Cipher;
 import javax.crypto.CipherOutputStream;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
-import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class Client {
