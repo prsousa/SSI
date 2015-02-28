887788c10420a893069499c5a9cd40d6a1772272
Paulo <Paulo@bl16-46-112.dsl.telepac.pt>
2015-02-28 16:56:01 +0000

    Communication encrypted using RC4 The datagrams are sent immediately without buffering to compleat a block

--- SSI/Cliente/Client.java
+++ SSI/Cliente/Client.java
@@ -3,15 +3,19 @@ package Cliente;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.crypto.Cipher;
 import javax.crypto.CipherOutputStream;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class Client {
@@ -21,20 +25,17 @@ public class Client {
 
         try {
             Socket soc = new Socket("localhost", Servidor.Server.PORT);
-//            PrintWriter pw = new PrintWriter( soc.getOutputStream(), true );
 
-            byte[] keyBytes = "123456789ABCDEF".getBytes();
+            byte[] keyBytes = "0123456789ABCDEF".getBytes();
             SecretKey key = new SecretKeySpec(keyBytes, 0, keyBytes.length, "RC4");
-
+            
+            // With RC4 the datagram is sent immediately without buffering to compleat a block
             Cipher cipher = Cipher.getInstance("RC4");
             cipher.init(Cipher.ENCRYPT_MODE, key);
 
             CipherOutputStream cos = new CipherOutputStream(soc.getOutputStream(), cipher);
 
             while (soc.isConnected()) {
-//                String line = br.readLine();
-//                pw.println( line );
-
                 int readed = br.read();
                 cos.write(readed);
                 cos.flush();
--- SSI/Servidor/HandleClient.java
+++ SSI/Servidor/HandleClient.java
@@ -13,35 +13,24 @@ import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
 public class HandleClient implements Runnable {
-
-//    private final BufferedReader br;
-//    private final PrintWriter pw;
     private final CipherInputStream cis;
     private final int order;
 
-    HandleClient(Socket soc, int order, Server server) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
-        byte[] keyBytes = "123456789ABCDEF".getBytes();
+    HandleClient(Socket soc, int order, Server server) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {        
+        byte[] keyBytes = "0123456789ABCDEF".getBytes();
         SecretKey key = new SecretKeySpec(keyBytes, 0, keyBytes.length, "RC4");
         
         Cipher cipher = Cipher.getInstance("RC4");
+        
         cipher.init(Cipher.DECRYPT_MODE, key);
         
         this.cis = new CipherInputStream(soc.getInputStream(), cipher);
-        
-//        this.br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-//        this.pw = new PrintWriter(s.getOutputStream(), true);
         this.order = order;
     }
 
     @Override
     public void run() {
         try {
-//            String line;
-//
-//            while ((line = br.readLine()) != null) {
-//                System.out.println(this.order + " " + line);
-//            }
-
             int test;
             while ((test = cis.read()) != -1) {
                 System.out.print((char) test);
