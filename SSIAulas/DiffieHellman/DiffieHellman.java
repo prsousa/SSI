a3391fc627396b3de90d1c736e51a8e2293bce91
Paulo Sousa <prsousa34@gmail.com>
2015-03-06 18:19:49 +0000

    Protocolo Diffie-Hellman - Acordo de Chaves Utilizando  KeyPairGenerator

--- SSIAulas/DiffieHellman/DiffieHellman.java
+++ SSIAulas/DiffieHellman/DiffieHellman.java
@@ -1,63 +1,78 @@
 package DiffieHellman;
 
-import java.math.BigInteger;
 import java.security.AlgorithmParameterGenerator;
 import java.security.AlgorithmParameters;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
 import java.security.spec.InvalidParameterSpecException;
-import java.util.Random;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.crypto.KeyAgreement;
+import javax.crypto.ShortBufferException;
+import javax.crypto.interfaces.DHPublicKey;
 import javax.crypto.spec.DHParameterSpec;
+import static sun.security.pkcs11.wrapper.Functions.toHexString;
 
 public class DiffieHellman {
-    private final BigInteger n;
-    private final BigInteger  g;
-    
-    private final BigInteger random;
-    private final BigInteger param;
-    
-    
-    public DiffieHellman(BigInteger n, BigInteger  g) {
-        this.n = n;
-        this.g = g;
-        
-        this.random = new BigInteger(1024, new Random());
-        this.param = g.modPow(this.random, n);
-    }
-    
-    public BigInteger getParam() {
-        return this.param;
-    }
-    
-    public BigInteger computeKey(BigInteger external) {
-        return external.modPow(random, n);
-    }
-    
-    
+
     public static void main(String[] args) {
         try {
-            AlgorithmParameterGenerator paramGen = AlgorithmParameterGenerator.getInstance("DiffieHellman");
+            DHParameterSpec dhSkipParamSpec;
+            AlgorithmParameterGenerator paramGen = AlgorithmParameterGenerator.getInstance("DH");
             paramGen.init(1024);
-            
             AlgorithmParameters params = paramGen.generateParameters();
-            DHParameterSpec dhSpec = (DHParameterSpec) params.getParameterSpec(DHParameterSpec.class);
+            dhSkipParamSpec = (DHParameterSpec) params.getParameterSpec(DHParameterSpec.class);
+
+            KeyPairGenerator aliceKpairGen = KeyPairGenerator.getInstance("DH");
+            aliceKpairGen.initialize(dhSkipParamSpec);
+            KeyPair aliceKpair = aliceKpairGen.generateKeyPair();
+            KeyAgreement aliceKeyAgree = KeyAgreement.getInstance("DH");
+            aliceKeyAgree.init(aliceKpair.getPrivate());
+
+            byte[] alicePubKeyEnc = aliceKpair.getPublic().getEncoded();
+
+            // Bob
+            KeyFactory bobKeyFac = KeyFactory.getInstance("DH");
+            X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(alicePubKeyEnc);
+            PublicKey alicePubKey = bobKeyFac.generatePublic(x509KeySpec);
+
+            DHParameterSpec dhParamSpec = ((DHPublicKey) alicePubKey).getParams();
+
+            KeyPairGenerator bobKpairGen = KeyPairGenerator.getInstance("DH");
+            bobKpairGen.initialize(dhParamSpec);
+            KeyPair bobKpair = bobKpairGen.generateKeyPair();
+
+            KeyAgreement bobKeyAgree = KeyAgreement.getInstance("DH");
+            bobKeyAgree.init(bobKpair.getPrivate());
+
+            byte[] bobPubKeyEnc = bobKpair.getPublic().getEncoded();
+
+            KeyFactory aliceKeyFac = KeyFactory.getInstance("DH");
+            x509KeySpec = new X509EncodedKeySpec(bobPubKeyEnc);
+            PublicKey bobPubKey = aliceKeyFac.generatePublic(x509KeySpec);
+            aliceKeyAgree.doPhase(bobPubKey, true);
+
+            bobKeyAgree.doPhase(alicePubKey, true);
+
+            byte[] aliceSharedSecret = aliceKeyAgree.generateSecret();
+            int aliceLen = aliceSharedSecret.length;
+
+            byte[] bobSharedSecret = new byte[aliceLen];
             
-            BigInteger n = dhSpec.getP();
-            BigInteger g = dhSpec.getG();
             
-            DiffieHellman Alice = new DiffieHellman(n, g);
-            DiffieHellman Bob = new DiffieHellman(n, g);
+            bobKeyAgree.generateSecret(bobSharedSecret, 0);
+
+            System.out.println( toHexString(aliceSharedSecret) );
+            System.out.println( toHexString(bobSharedSecret) );
             
-            BigInteger publicAliceParam = Alice.getParam();
-            BigInteger publicBobParam = Bob.getParam();
-            
-            BigInteger aliceKey = Alice.computeKey(publicBobParam);
-            BigInteger bobKey = Bob.computeKey(publicAliceParam);
-            
-            System.out.println(aliceKey);
-            System.out.println(bobKey);
-        } catch (NoSuchAlgorithmException | InvalidParameterSpecException ex) {
+        } catch (NoSuchAlgorithmException | InvalidParameterSpecException | InvalidAlgorithmParameterException | InvalidKeyException | InvalidKeySpecException | IllegalStateException | ShortBufferException ex) {
             Logger.getLogger(DiffieHellman.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
